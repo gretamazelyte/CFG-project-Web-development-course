007d7c7b9232fe57a84225c5faf4d33d6c4bcfe2
gretamazelyte <64148668+gretamazelyte@users.noreply.github.com>
2020-06-10 15:20:05 +0100

    js cats update

--- js/app.js
+++ js/app.js
@@ -5,3 +5,24 @@ function changePic1() {
   function changePic2(){
     document.getElementById("catover").src = "../images/cat.jpg";
   }
+
+function changePic3() {
+    document.getElementById("caulicat").src = "../images/caulicat.jpg";
+  }
+  function changePic4(){
+    document.getElementById("caulicat").src = "../images/cauli.jpg";
+  }
+
+function changePic5() {
+    document.getElementById("muffincat").src = "../images/muffincat.jpg";
+  }
+  function changePic6(){
+    document.getElementById("muffincat").src = "../images/muffins.jpg";
+  }
+
+function changePic7() {
+    document.getElementById("brusselcat").src = "../images/brusselcat.jpg";
+  }
+  function changePic8(){
+    document.getElementById("brusselcat").src = "../images/miso.png";
+  }
